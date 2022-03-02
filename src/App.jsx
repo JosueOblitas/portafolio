dd12be1a496bd1b5672f2e1916e6e5bf083529f7
Javier Oblitas Calvo <josueoblitas15@gmail.com>
2022-03-02 01:26:47 -0500

    add code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext} from "react";
 import About from "./components/about/about";
 import Contact from "./components/contact/contact";
 import Intro from "./components/intro/intro";
